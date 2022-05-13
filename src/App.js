810c456a4dc3a87f7fb76a9af0a14bf1e445afb7
Brandon Sitz <Brandonsitz17@gmail.com>
2022-05-13 16:17:16 -0500

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<Layout></Layout>}>
           <Route index element={<Home></Home>} />
           <Route path='about' element={<About></About>} />
+          <Route path='contact' element={<Contact></Contact>} />
         </Route>
     </Routes>
     </>
---
+++ src/components/Contact/index.js
@@ -0,0 +1,52 @@
+import './index.scss'
+
+const Contact = () => {
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>Contact me</h1>
+          <p>Lorem Ipsum: contact</p>
+          <div className="contact-form">
+            <form>
+              <ul>
+                <li className="half">
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    required
+                  ></input>
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  ></input>
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  ></input>
+                </li>
+                <li>
+                    <textarea placeholder='Message' name='message' required></textarea>
+                </li>
+                <li>
+                    <input type='submit' classname='flat-button' value='SEND'></input>
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Contact
