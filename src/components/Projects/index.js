2575ba78af8e1b3cdef1f487ded4db765cb08c98
Brandon Sitz <Brandonsitz17@gmail.com>
2022-06-11 18:36:54 -0400

    Project Section Added

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,11 +1,39 @@
 import './index.scss'
+import ProjectData from '../../data/projects.json'
 
 const Projects = () => {
+  const renderProjects = (projects) => {
     return (
-        <div>
-            <h1>Projects</h1>
-        </div>
+      <div className="images-container">
+        {projects.map((port, idx) => {
+          return (
+            <div className="image-box" key={idx}>
+              <img
+                src={port.cover}
+                alt="projects"
+                className="projects-image"
+              ></img>
+              <div className="content">
+                <p className="title">{port.title}</p>
+                <h4 className="description">{port.description}</h4>
+                <button className="btn" 
+                onClick={() => window.open(port.url)}>
+                  View
+                </button>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     )
+  }
+
+  return (
+    <div className="container projects-page">
+      <h1 className="title">Projects</h1>
+      <div>{renderProjects(ProjectData.projects)}</div>
+    </div>
+  )
 }
 
-export default Projects;
+export default Projects
