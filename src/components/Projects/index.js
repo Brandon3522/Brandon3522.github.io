0c0f978f68118836df9c04279f67be826d5e1eea
Brandon Sitz <Brandonsitz17@gmail.com>
2022-06-16 16:28:17 -0400

    Update

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -16,8 +16,7 @@ const Projects = () => {
               <div className="content">
                 <p className="title">{port.title}</p>
                 <h4 className="description">{port.description}</h4>
-                <button className="btn" 
-                onClick={() => window.open(port.url)}>
+                <button className="btn" onClick={() => window.open(port.url)}>
                   View
                 </button>
               </div>
