fc8c7296838bb5288e08e19d6d9cdb7129154575
Brandon Sitz <Brandonsitz17@gmail.com>
2022-06-18 20:46:53 -0500

    Projects / about me update

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,9 +8,27 @@ const About = () => {
                 <h1>
                     About me
                 </h1>
-                <p>lorem Ipsum</p>
-                <p>lorem Ipsum</p>
-                <p>lorem Ipsum</p>
+                <p>
+                    Hello. My name is Brandon and my interest in software devlopment began in 2018 when I 
+                    started building computers.
+                </p>
+                <p>
+                    I am currently a student at Austin Peay State University. 
+                    I am pursuing a degree in Computer Science.
+                </p>
+                <p>
+                    I have extensive knowledge of programming fundamentals 
+                    and a solid foundation in software engineering principles.
+                </p>
+                <p>Technologies used:</p>
+
+                <ul>
+                    <li> C# </li>
+                    <li> Java </li>
+                    <li> Python </li>
+                    <li> JavaScript </li>
+                    <li> TypeScript </li>
+                </ul>
             </div>
         </div>
     )
