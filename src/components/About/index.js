54814ba3a8a0fb584df35a2ab419b4815d230ec9
Brandon3522 <77870751+Brandon3522@users.noreply.github.com>
2023-09-28 08:51:27 -0400

    Update about me page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,10 +7,9 @@ const About = () => {
         <h1>About me</h1>
         <p className="p1">
           Hello, my name is Brandon, and I'm a software developer with a passion
-          for programming and a desire to constantly learn and grow. My journey
-          in software development started in 2017 when I built my first
-          computer, and since then, I have been captivated by the endless
-          possibilities that programming offers.
+          for programming. My journey in software development started in 2017
+          when I built my first computer, and since then, I have been captivated
+          by the endless possibilities that programming offers.
         </p>
         <p className="p2">
           I recently graduated from Austin Peay State University with a
@@ -23,10 +22,10 @@ const About = () => {
           In addition to my academic achievements, I have experience working on
           a variety of programming projects, including developing desktop
           applications and web-based systems. I am proficient in C#, Python, and
-          JavaScript and have experience working with React.js. When I'm not
+          JavaScript. I also have experience working with React.js and .NET Core. When I'm not
           programming, I enjoy playing guitar and listening to music.
         </p>
-        <p className="p4">Languages / Technologies used:</p>
+        <p className="p4">Languages:</p>
 
         <ul className="ul-list">
           <li> C# </li>
@@ -35,8 +34,19 @@ const About = () => {
           <li> HTML / CSS </li>
           <li> JavaScript </li>
           <li> TypeScript </li>
+		  <li> SQL </li>
+		  <li> NoSQL </li>
+        </ul>
+
+		<p className="p5">Frameworks:</p>
+
+        <ul className="ul-list">
           <li> React.js </li>
-          <li> Firebase </li>
+		  <li> Firebase </li>
+		  <li> .NET Core </li>
+		  <li> Windows Presentation Foundation (WPF) </li>
+		  <li> JUnit </li>
+          <li> Node.js </li>
         </ul>
       </div>
     </div>
